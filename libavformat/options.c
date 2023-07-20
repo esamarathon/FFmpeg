ccc92b804ebb165f8ce9df9210ca92afc752b1f6
Timo Rothenpieler <timo@rothenpieler.org>
2023-07-20 19:05:26 +0200

    DO NOT MERGE: silence "Opening XYZ"-Spam

--- libavformat/options.c
+++ libavformat/options.c
@@ -147,9 +147,9 @@ static int io_open_default(AVFormatContext *s, AVIOContext **pb,
         s->iformat && !strcmp(s->iformat->name, "image2") ||
         s->oformat && !strcmp(s->oformat->name, "image2")
     ) {
-        loglevel = AV_LOG_DEBUG;
+        loglevel = AV_LOG_TRACE;
     } else
-        loglevel = AV_LOG_INFO;
+        loglevel = AV_LOG_TRACE;
 
     av_log(s, loglevel, "Opening \'%s\' for %s\n", url, flags & AVIO_FLAG_WRITE ? "writing" : "reading");
 
